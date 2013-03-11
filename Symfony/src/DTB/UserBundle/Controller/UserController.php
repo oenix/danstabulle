5e3dd4afe3a1bb5c5c1f46690bbbaee8afa1b5f5
Arthur Bosquette <arthur.bosquette@epita.fr>
2013-03-11 17:38:33 +0100

    User profile

---
+++ Symfony/src/DTB/UserBundle/Controller/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace DTB\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use DTB\UserBundle\Entity\User;
+
+class UserController extends Controller {
+
+    /**
+     * Show the requested user
+     */
+    public function showUserAction($id) {
+        $repository = $this->getDoctrine()
+                     ->getManager()
+                     ->getRepository('DTBUserBundle:User');
+        
+        $user = $repository->find($id);
+        var_dump($user);
+    }
+
+}
+
+?>
