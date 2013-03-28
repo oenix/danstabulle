3cc8b5cdf4a023727a7b70dc762712f22426d0e7
Arthur Bosquette <arthur.bosquette@epita.fr>
2013-03-28 16:03:16 +0100

    Creating Bd Bundle

--- Symfony/app/AppKernel.php
+++ Symfony/app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new DTB\UserBundle\DTBUserBundle(),
             new DTB\OfflineBundle\DTBOfflineBundle(),
+            new DTB\BdBundle\DTBBdBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ Symfony/src/DTB/BdBundle/DTBBdBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace DTB\BdBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DTBBdBundle extends Bundle
+{
+}
