3b038a5c24bfd60254745d9d87b43f97fe28a398
Victor Carmouze <victor.carmouze@gmail.com>
2013-04-02 18:03:07 +0200

    Explorer file
    Wtf id beta !!

--- src/html/js/danstabulle.js
+++ src/html/js/danstabulle.js
@@ -1,3 +1,15 @@
 $(function(){
 	$(".pop").popover({placement:'right', trigger:'hover'});
 });
+
+function cacher(id)
+{
+	document.getElementById(id).style.display="none";
+	document.getElementById(id + "_complete").style.display="";
+}
+
+function afficher(id)
+{
+	document.getElementById(id).style.display="block";
+	document.getElementById(id + "_complete").style.display="none";
+}
