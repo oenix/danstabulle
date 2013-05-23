cd60e0b2c89b6b69945c4ec3db641356f11454e8
Arthur Bosquette <arthur.bosquette@epita.fr>
2013-05-23 14:45:08 +0200

    Adding Image Entity linked to Planche

--- Symfony/src/DTB/BdBundle/Entity/Planche.php
+++ Symfony/src/DTB/BdBundle/Entity/Planche.php
@@ -22,9 +22,21 @@ class Planche
     private $id;
     
     /**
+     * @var integer
+     *
+     * @ORM\Column(name="page", type="integer", nullable=true)
+     */
+    private $page;
+    
+    /**
      * @ORM\ManytoOne(targetEntity="DTB\BdBundle\Entity\BandeDessinee", inversedBy="planches", cascade={"persist"})
      */
     private $bandeDessinee;
+    
+    /**
+     * @ORM\OneToMany(targetEntity="DTB\BdBundle\Entity\Image", mappedBy="planche")
+     */
+    private $images;
 
 
     /**
@@ -58,5 +70,68 @@ class Planche
     public function getBandeDessinee()
     {
         return $this->bandeDessinee;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->images = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+    
+    /**
+     * Set page
+     *
+     * @param integer $page
+     * @return Planche
+     */
+    public function setPage($page)
+    {
+        $this->page = $page;
+    
+        return $this;
+    }
+
+    /**
+     * Get page
+     *
+     * @return integer 
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * Add images
+     *
+     * @param \DTB\BdBundle\Entity\Image $images
+     * @return Planche
+     */
+    public function addImage(\DTB\BdBundle\Entity\Image $images)
+    {
+        $this->images[] = $images;
+    
+        return $this;
+    }
+
+    /**
+     * Remove images
+     *
+     * @param \DTB\BdBundle\Entity\Image $images
+     */
+    public function removeImage(\DTB\BdBundle\Entity\Image $images)
+    {
+        $this->images->removeElement($images);
+    }
+
+    /**
+     * Get images
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getImages()
+    {
+        return $this->images;
     }
 }
