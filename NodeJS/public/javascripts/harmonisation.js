610b6b72d26325935fc13233ded362b65ed5c796
Victor Carmouze <victor.carmouze@gmail.com>
2013-06-23 12:49:53 +0200

    Add Opacity for color picker

--- NodeJS/public/javascripts/harmonisation.js
+++ NodeJS/public/javascripts/harmonisation.js
@@ -19,6 +19,10 @@ function isHex(val){
         return true;
     }
 }
+function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)}
+function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)}
+function hexToB(h) {return parseInt((cutHex(h)).substring(4,6),16)}
+function cutHex(h) {return (h.charAt(0)=="#") ? h.substring(1,7):h}
 
 function delColor() {
 		var option = document.getElementById("colorHexa").value.toLowerCase();
@@ -33,7 +37,12 @@ function delColor() {
 
 function selectColor() {
 	var selectedColor = getSelectValue('color');
-	document.getElementById("color").style.backgroundColor = selectedColor;
+	var R = hexToR(selectedColor);
+	var G = hexToG(selectedColor);
+	var B = hexToB(selectedColor)
+	
+	document.getElementById("color").style.backgroundColor = "rgba(" + R + "," + G + "," + B + "," + getSelectValue('opacity') / 100 + ")";
+	//selectedColor;
 	document.getElementById("colorHexa").style.backgroundColor = selectedColor;
 	selectedColor = selectedColor.substring(1);
 	document.getElementById("colorHexa").value = selectedColor.toUpperCase();
